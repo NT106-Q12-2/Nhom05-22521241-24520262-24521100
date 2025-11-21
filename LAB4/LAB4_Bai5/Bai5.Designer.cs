cd4b56848a3fd1f472c07daa1a8c9e6814bd3aec
Dainh756 <24520262@gm.uit.edu.vn>
2025-11-21 22:14:28 +0700

    Hoan thanh bai 5 lab4

--- LAB4/LAB4_Bai5/Bai5.Designer.cs
+++ LAB4/LAB4_Bai5/Bai5.Designer.cs
@@ -28,12 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            txtb_url = new TextBox();
+            txtb_username = new TextBox();
+            txtb_password = new TextBox();
+            btn_login = new Button();
+            rtb_content = new RichTextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // txtb_url
+            // 
+            txtb_url.Location = new Point(143, 12);
+            txtb_url.Name = "txtb_url";
+            txtb_url.Size = new Size(499, 27);
+            txtb_url.TabIndex = 0;
+            // 
+            // txtb_username
+            // 
+            txtb_username.Location = new Point(143, 76);
+            txtb_username.Name = "txtb_username";
+            txtb_username.Size = new Size(289, 27);
+            txtb_username.TabIndex = 1;
+            // 
+            // txtb_password
+            // 
+            txtb_password.Location = new Point(143, 135);
+            txtb_password.Name = "txtb_password";
+            txtb_password.Size = new Size(289, 27);
+            txtb_password.TabIndex = 2;
+            // 
+            // btn_login
+            // 
+            btn_login.Location = new Point(465, 76);
+            btn_login.Name = "btn_login";
+            btn_login.Size = new Size(177, 86);
+            btn_login.TabIndex = 3;
+            btn_login.Text = "LOGIN";
+            btn_login.UseVisualStyleBackColor = true;
+            btn_login.Click += btn_login_Click;
+            // 
+            // rtb_content
+            // 
+            rtb_content.Location = new Point(41, 178);
+            rtb_content.Name = "rtb_content";
+            rtb_content.Size = new Size(601, 242);
+            rtb_content.TabIndex = 4;
+            rtb_content.Text = "";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(55, 15);
+            label1.Name = "label1";
+            label1.Size = new Size(35, 20);
+            label1.TabIndex = 5;
+            label1.Text = "URL";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(55, 79);
+            label2.Name = "label2";
+            label2.Size = new Size(75, 20);
+            label2.TabIndex = 6;
+            label2.Text = "Username";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(55, 138);
+            label3.Name = "label3";
+            label3.Size = new Size(70, 20);
+            label3.TabIndex = 7;
+            label3.Text = "Password";
+            // 
+            // Bai5
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(698, 450);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(rtb_content);
+            Controls.Add(btn_login);
+            Controls.Add(txtb_password);
+            Controls.Add(txtb_username);
+            Controls.Add(txtb_url);
+            Name = "Bai5";
+            Text = "BAI5";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtb_url;
+        private TextBox txtb_username;
+        private TextBox txtb_password;
+        private Button btn_login;
+        private RichTextBox rtb_content;
+        private Label label1;
+        private Label label2;
+        private Label label3;
     }
 }
--- LAB4/LAB4_Bai5/Bai5.cs
+++ LAB4/LAB4_Bai5/Bai5.cs
@@ -1,3 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
 namespace LAB4_Bai5
 {
     public partial class Bai5 : Form
@@ -5,6 +17,71 @@ namespace LAB4_Bai5
         public Bai5()
         {
             InitializeComponent();
+            txtb_url.Text = "https://nt106.uitiot.vn/auth/token";
+            txtb_password.PasswordChar = '*';
+        }
+        private async void btn_login_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string url = txtb_url.Text;
+                string username = txtb_username.Text;
+                string password = txtb_password.Text;
+
+                if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+                {
+                    MessageBox.Show("Vui lòng nhập đầy đủ Username và Password!");
+                    return;
+                }
+
+                using (HttpClient client = new HttpClient())
+                {
+                    // Tạo nội dung gửi đi dạng Form-Data (MultipaartFormDataContent)
+                    // Đây là yêu cầu bắt buộc của API bài này
+                    MultipartFormDataContent content = new MultipartFormDataContent();
+                    content.Add(new StringContent(username), "username");
+                    content.Add(new StringContent(password), "password");
+
+                    // Hiển thị thông báo đang xử lý
+                    rtb_content.Text = "Đang gửi yêu cầu đăng nhập...";
+
+                    // Gửi yêu cầu POST (sử dụng await để không đơ giao diện)
+                    HttpResponseMessage response = await client.PostAsync(url, content);
+
+                    // Đọc nội dung phản hồi từ Server
+                    string responseString = await response.Content.ReadAsStringAsync();
+
+                    // Phân tích chuỗi JSON trả về thành đối tượng JObject
+                    JObject jsonResponse = JObject.Parse(responseString);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // Lấy Token từ JSON
+                        string tokenType = jsonResponse["token_type"].ToString();
+                        string accessToken = jsonResponse["access_token"].ToString();
+
+                        rtb_content.Text = "ĐĂNG NHẬP THÀNH CÔNG!\n\n";
+                        rtb_content.Text += $"Token Type: {tokenType}\n";
+                        rtb_content.Text += $"Access Token: {accessToken}\n";
+
+                        MessageBox.Show("Đăng nhập thành công!");
+                    }
+                    else
+                    {
+                        string detail = jsonResponse["detail"]?.ToString() ?? "Lỗi không xác định";
+
+                        rtb_content.Text = "ĐĂNG NHẬP THẤT BẠI.\n\n";
+                        rtb_content.Text += responseString;
+
+                        MessageBox.Show("Đăng nhập thất bại: " + detail);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi hệ thống: " + ex.Message);
+                rtb_content.Text = "Lỗi: " + ex.Message;
+            }
         }
     }
 }
